ca57b675033455f08199b375631de26308b403d8
markbomby <161911557+markbomby@users.noreply.github.com>
2026-09-22 14:14:33 +0700

    Add src/adder4_nodft.v

---
+++ src/adder4_nodft.v
@@ -0,0 +1,19 @@
+`default_nettype none
+
+module adder4_nodft (
+    input  wire [3:0] a,
+    input  wire [3:0] b,
+    input  wire       cin,
+    output wire [3:0] sum,
+    output wire       cout
+);
+    wire [4:0] result;
+
+    assign result = {1'b0, a} + {1'b0, b} + cin;
+    assign sum    = result[3:0];
+    assign cout   = result[4];
+endmodule
+
+`default_nettype wire
+
+
